5a3e36f0e11ce137c6f68e9b02010f19c489b134
João Araújo <joaoaraujo.78@gmail.com>
2022-04-12 20:05:12 +0100

    functional version

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 const email = document.getElementById("mail");
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
+const phone = document.getElementById("phone");
+const pwd = document.getElementById("password");
+const sndpwd = document.getElementById("confirmpassword");
 
+firstName.addEventListener("invalid", function (event) {
+  if (firstName.validity.valueMissing) {
+    firstName.setCustomValidity("Please fill this field.");
+  } else {
+    firstName.setCustomValidity("");
+  }
+});
 
-email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
+lastName.addEventListener("invalid", function (event) {
+  if (lastName.validity.valueMissing) {
+    lastName.setCustomValidity("Please fill this field.");
+  } else {
+    lastName.setCustomValidity("");
+  }
+});
+
+email.addEventListener("invalid", function (event) {
+  if (email.validity.typeMismatch || email.validity.valueMissing) {
     email.setCustomValidity("I am expecting an e-mail address!");
   } else {
     email.setCustomValidity("");
   }
 });
 
-firstName.addEventListener("input", function (event) {
-  if (firstName.validity.valueMissing) {
-    firstName.setCustomValidity("Please fill this field 123.");
-    firstName.textContent='Hello';
-    firstName.reportValidity();
+phone.addEventListener("invalid", function (event) {
+  if (phone.validity.valueMissing) {
+    phone.setCustomValidity("Please fill this field.");
   } else {
-    firstName.setCustomValidity("");
+    phone.setCustomValidity("");
   }
 });
 
-lastName.addEventListener("input", function (event) {
-  if (lastName.validity.valueMissing) {
-    lastName.setCustomValidity("Please fill this field.");
-    lastName.reportValidity();
+pwd.addEventListener("invalid", function (event) {
+  if (pwd.validity.valueMissing) {
+    pwd.setCustomValidity("Please fill this field.");
   } else {
-    lastName.setCustomValidity("");
+    pwd.setCustomValidity("");
   }
 });
 
+sndpwd.addEventListener("invalid", function (event) {
+  if (sndpwd.validity.valueMissing) {
+    sndpwd.setCustomValidity("Please fill this field.");
+  } else {
+    sndpwd.setCustomValidity("");
+  }
+});
